dcd7e5abb2937b0b29d932506a186f6a4d9be416
Mykyta Melnyk <liswelus@gmail.com>
2023-02-28 18:31:34 +0100

    Fixed tailwind styles generation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-      "./templates/_docs/**/*.html.twig"
+      "./templates/**/*.html.twig"
   ],
   theme: {
     extend: {},
