af04c6b790ce582fa50b0282970cf9fef7ea6443
Mykyta Melnyk <liswelus@gmail.com>
2023-03-05 15:18:54 +0100

    Display method route if exists

--- src/Resources/public/docs.js
+++ src/Resources/public/docs.js
@@ -1,1 +1,19 @@
-"use strict";(self.webpackChunknvdoc_bundle=self.webpackChunknvdoc_bundle||[]).push([[163],{482:()=>{}},e=>{var n;n=482,e(e.s=n)}]);
+"use strict";
+(self["webpackChunknvdoc_bundle"] = self["webpackChunknvdoc_bundle"] || []).push([["docs"],{
+
+/***/ "./assets/index.ts":
+/*!*************************!*\
+  !*** ./assets/index.ts ***!
+  \*************************/
+/***/ (() => {
+
+
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./assets/index.ts"));
+/******/ }
+]);
